c855d6f986d52b763da990cc59a3c266d3ee1133
cleber <cleber@dronemapp.com>
2017-07-18 09:18:41 -0300

    update domain_aliases accordingly to what is done on Netlify website

--- netlify.py
+++ netlify.py
@@ -15,19 +15,14 @@ def get(path):
     return requests.get(url)
 
 
-def patch(path, data):
+def put(path, data):
     global BASE_URL
     global TOKEN
     url = os.path.join(BASE_URL, path + '?access_token={}'.format(TOKEN))
-    return requests.patch(url, data)
+    return requests.put(url, json=data)
 
-
-def aliases_present(name, access_token, site_id, aliases):
-    global TOKEN
-    TOKEN = access_token
-
+def get_current_aliases(site_id, access_token):
     site_path = 'sites/{}'.format(site_id)
-
     response = get(site_path)
     try:
         response.raise_for_status()
@@ -40,7 +35,16 @@ def aliases_present(name, access_token, site_id, aliases):
         }
 
     response_data = response.json()
-    current_aliases = set(response_data['domain_aliases'])
+    return set(response_data['domain_aliases'])
+
+
+def aliases_present(name, access_token, site_id, aliases):
+    global TOKEN
+    TOKEN = access_token
+
+    site_path = 'sites/{}'.format(site_id)
+
+    current_aliases = get_current_aliases(site_id, access_token)
     updated_aliases = set(current_aliases)
 
     for alias_name in aliases:
@@ -55,20 +59,33 @@ def aliases_present(name, access_token, site_id, aliases):
         }
 
     new_aliases = updated_aliases - current_aliases
-    response = patch(site_path, {"domain_aliases": list(updated_aliases)})
-    try:
-        response.raise_for_status()
-    except HTTPError as ex:
-        return {
-            "name": name,
-            "changes": {"changes": None},
-            "comment": "{} ({})".format(ex, ex.response.content),
-            "result": False
-        }
 
-    return {
+    success_return = {
         "name": name,
         "comment": "{} new aliases created".format(len(new_aliases)),
         "changes": {"subdomain aliases created": list(new_aliases)},
         "result": True
     }
+
+    payload = {"domain_aliases": list(updated_aliases)}
+    response = put(site_path, payload)
+    try:
+        response.raise_for_status()
+    except HTTPError as ex:
+        try:
+            new_current_aliases = get_current_aliases(site_id, access_token)
+        except:
+            pass
+        else:
+            if new_current_aliases == updated_aliases:
+                success_return['changes']['status_code'] = response.status_code
+                return success_return
+
+        return {
+            "name": name,
+            "changes": {"changes": None},
+            "comment": "{} ({}) : {}".format(ex, ex.response.content, payload),
+            "result": False
+        }
+
+    return success_return
